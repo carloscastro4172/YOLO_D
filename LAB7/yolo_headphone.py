da6f6149024f0839992dd94d68a5445164bba11d
Carlos_182b <105805112+carloscastro4172@users.noreply.github.com>
2025-11-07 14:15:31 -0500

    LAB7

---
+++ LAB7/yolo_headphone.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+model = YOLO('/home/carlos/IA/LAB6/LAB6.2/TRAIN_YOLO/best.pt')
+results = model.predict(source=0,  # webcam o cámara conectada
+                        imgsz=640,
+                        conf=0.5)
+results.show()
